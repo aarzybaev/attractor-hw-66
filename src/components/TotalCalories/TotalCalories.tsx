5fab47712cb8a45708a20fe8d509529e016111d4
Askhad Arzybaev <aarzybaev@gmail.com>
2024-02-19 23:30:55 +0600

    inserted semicolon && reformatted code

--- src/components/TotalCalories/TotalCalories.tsx
+++ src/components/TotalCalories/TotalCalories.tsx
@@ -5,15 +5,16 @@ import {useNavigate} from 'react-router-dom';
 interface Props {
   meals: Meal[];
 }
+
 const TotalCalories: React.FC<Props> = ({meals}) => {
   const navigation = useNavigate();
-  const getTotalCalories = (items: Meal[]) => items.reduce((acc, cur) => acc + cur.calorie, 0)
+  const getTotalCalories = (items: Meal[]) => items.reduce((acc, cur) => acc + cur.calorie, 0);
   return (
     <div className="d-flex align-items-center justify-content-between">
       <div>
         Total calories: <span className="fw-bold">{getTotalCalories(meals)} kcal</span>
       </div>
-      <button className="btn btn-primary btn-sm" onClick={()=> navigation('/meals/new')}>Add new meal</button>
+      <button className="btn btn-primary btn-sm" onClick={() => navigation('/meals/new')}>Add new meal</button>
     </div>
   );
 };
