7d06c5e06024c6d55ead8a11496ff9bd218842a5
Askhad Arzybaev <aarzybaev@gmail.com>
2024-02-19 23:33:12 +0600

    wrote routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import {Route, Routes} from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Meals from './containers/Meals/Meals';
+import PageNotFound from './containers/PageNotFound/PageNotFound';
+import NewMeal from './containers/NewMeal/NewMeal';
 
 const App = () => (
-  <>
-    <h3>Hello</h3>
-  </>
+  <Layout>
+    <Routes>
+      <Route path="/" element={<Meals/>}/>
+      <Route path="/meals/new" element={<NewMeal/>}/>
+      <Route path="/meals/edit/:id" element={<NewMeal isEdit={true}/>}/>
+      <Route path="*" element={<PageNotFound/>}/>
+    </Routes>
+  </Layout>
 );
 
 export default App;
